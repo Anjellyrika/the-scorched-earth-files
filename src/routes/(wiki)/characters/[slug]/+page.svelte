d1ea3bfebf3b3edd6e1fb5114fc0e0062608c570
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2025-04-12 00:24:36 +0800

    feat(ui): add sample character page

---
+++ src/routes/(wiki)/characters/[slug]/+page.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	// Define infos here temporarily
+	import CharacterQuote from '$lib/components/wiki/CharacterQuote.svelte';
+	import CharlesPortrait from '$lib/content (temporary)/photos (temporary)/chase-s3.jpg';
+	import ChaseHS from '$lib/content (temporary)/photos (temporary)/chase-hs.jpg';
+	import ChasePage from '$lib/content (temporary)/Chase.json';
+	import ChaseS1 from '$lib/content (temporary)/photos (temporary)/chase-s1.png';
+	import ChaseS2 from '$lib/content (temporary)/photos (temporary)/Scar.png';
+	import Infobox from '$lib/components/wiki/InfoboxCharacter.svelte';
+	import TableOfContents from '$lib/components/wiki/TableOfContents.svelte';
+
+	let portrait = CharlesPortrait;
+	let gallery = [ChaseHS, ChaseS1, ChaseS2, CharlesPortrait];
+
+	const displayName = ChasePage.displayname;
+	const bio = ChasePage.bio;
+	const role = ChasePage.role;
+	const main = ChasePage.mainContent;
+</script>
+
+<main>
+	<section class="flex flex-row-reverse gap-12">
+		<Infobox image={portrait} {bio} {role} />
+		<section class="space-y-4">
+			<h1>{displayName}</h1>
+			<CharacterQuote>{ChasePage.characterQuote}</CharacterQuote>
+			<p class="text-surface-300 leading-relaxed">{ChasePage.shortIntro}</p>
+			<TableOfContents contents={main} />
+		</section>
+	</section>
+	<section>
+		{#each Object.entries(main) as [heading, content] (heading)}
+			<div class="my-12">
+				<h2 id={heading} class="border-surface-300 mb-2 w-fit border-b-1">
+					{heading.toUpperCase()}
+				</h2>
+				{#if heading === 'introduction'}
+					<p class="text-surface-300 leading-relaxed">{content}</p>
+				{:else if heading === 'trivia' && Array.isArray(content)}
+					<ul class="list-inside list-disc">
+						{#each content as trivia (trivia)}
+							<li class="text-surface-300">{trivia}</li>
+						{/each}
+					</ul>
+				{:else}
+					{#each Object.entries(content) as [subheading, innerContent] (subheading)}
+						<div class="my-8">
+							<h3 class="text-surface-200 border-surface-600 mb-2 w-fit border-b-1">
+								{subheading.toUpperCase()}
+							</h3>
+							<p class="text-surface-300 leading-relaxed">{innerContent}</p>
+						</div>
+					{/each}
+				{/if}
+			</div>
+		{/each}
+	</section>
+	<section>
+		<h2 class="border-surface-300 mb-2 w-fit border-b-1">GALLERY</h2>
+		<div class="grid grid-cols-4 gap-4">
+			{#each gallery as photo (photo)}
+				<img
+					src={photo}
+					alt=""
+					class="hover:border-surface-300 hover:bg-surface-300 max-h-68 w-full object-cover object-top transition-transform hover:scale-105 hover:border-9 hover:pb-8 hover:shadow-xl odd:hover:rotate-2 even:hover:-rotate-2"
+				/>
+			{/each}
+		</div>
+	</section>
+</main>
