c1823db2551e7407aac41676d90dab3a42610d41
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2025-04-12 01:16:28 +0800

    feat(ui): add greeting

---
+++ src/routes/gift/+page.svelte
@@ -0,0 +1,17 @@
+<main class="bg-secondary-700 flex min-h-screen flex-col items-center gap-8 p-20 text-center">
+	<h1 class="font-title tracking-wider md:text-5xl">(ADVANCED) HAPPY BIRTHDAY, JOSHUA!</h1>
+	<h4 class="text-surface-200">I believe that Scorched Earth is love, Scorched Earth is life.</h4>
+	<h2 class="text-surface-200">Scorched Earth is the gift that keeps on giving.</h2>
+	<p class="text-surface-300">
+		I've made a lot of happy memories playing Scorched Earth with you guys. It's still a work in
+		progress, but I hope you enjoy this little project as a token of my gratitude—for the world
+		that you poured so much into for all of us to enjoy.
+	</p>
+	<a class="bg-surface-950 hover:border-tertiary-500 p-4 hover:border-l-4" href="/"
+		>➤ Go to Home</a
+	>
+	<p class="text-surface-300">P.S. Also view it on a laptop!</p>
+	<p class="text-surface-300">
+		P.P.S. I'll remove this greeting page after your birthday is done
+	</p>
+</main>
